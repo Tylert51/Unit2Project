03144d11f65413d51afc7c265263c5b395c81464
tyler <ttang7910@bths.edu>
2022-10-12 11:59:27 -0400

    written most of code for runner class and LinearEquation

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,6 +1,25 @@
 import java.util.Scanner;
 public class EquationRunner {
     public static void main(String[] args) {
+        //user input; will store users coordinate points and store their values in variables
+
+        Scanner s = new Scanner(System.in);
         System.out.println("Hey, what is your name?");
+        String name = s.nextLine();
+        System.out.println("Hey " + name + "!\nEnter your first Coordinate Point: ");
+        String coord1 = s.nextLine();
+        System.out.println("Enter your second Coordinate Point: ");
+        String coord2 = s.nextLine();
+
+        //instantiating the object
+        LinearEquation equation1 = new LinearEquation(coord1, coord2);
+
+        //output
+        System.out.println(equation1.toString());
+
+        //calculation and question of x3
+        System.out.println("Enter a third x-value: ");
+        double x3 = s.nextDouble();
+        System.out.println("Solved coordinate point is: " + equation1.calcThirdPoint(x3));
     }
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,4 +1,5 @@
 public class LinearEquation {
+    //instance variables
     int x1;
     int y1;
     int x2;
@@ -6,6 +7,7 @@ public class LinearEquation {
     double slope;
     String coord1;
     String coord2;
+    //constructor
     public LinearEquation(String coord1, String coord2) {
         this.coord1 = coord1;
         this.coord2 = coord2;
@@ -17,6 +19,7 @@ public class LinearEquation {
 
     }
 
+    //calculates slope
     public String slopeCalc() {
         int rise = y2 - y1;
         int run = x2 - x1;
@@ -24,47 +27,57 @@ public class LinearEquation {
         return rise + "/" + run;
     }
 
+    //calculates y-intercept
     public double yInt() {
         return y1 - (slope * x1);
 
     }
 
+    //calculates the equation in slope intercept from
     public String slopeIntForm() {
         return "y = " + slope + "x" + yInt();
     }
 
+    //calculates the distance between the two points
     public double distanceCalc() {
         return Math.sqrt((Math.pow((x2 - x1),2) + Math.pow((y2-y1),2)));
     }
 
+    //calculates the coordinate pair for a third x value
     public String calcThirdPoint (double x3) {
         double y3 = (slope * x3) + yInt();
         return "(" + x3 + "," + y3 + ")";
     }
 
+    //gets the integer value of x1 from coord1
     public int getCoordX1 () {
         int commaPlace = coord1.indexOf(",");
         int closeParenthesisPlace = coord1.indexOf(")");
         return Integer.parseInt(coord1.substring(1, commaPlace));
     }
 
+    //gets the integer value of y1 from coord1
     public int getCoordY1 () {
         int commaPlace = coord1.indexOf(",");
         int closeParenthesisPlace = coord1.indexOf(")");
         return Integer.parseInt(coord1.substring(commaPlace+1, closeParenthesisPlace));
     }
 
+    //gets the integer value of x2 from coord2
     public int getCoordX2 () {
         int commaPlace = coord2.indexOf(",");
         int closeParenthesisPlace = coord2.indexOf(")");
         return Integer.parseInt(coord2.substring(1, commaPlace));
     }
 
+    //gets the integer value of y2 from coord2
     public int getCoordY2 () {
         int commaPlace = coord2.indexOf(",");
         int closeParenthesisPlace = coord2.indexOf(")");
         return Integer.parseInt(coord2.substring(commaPlace+1, closeParenthesisPlace));
     }
+
+    //prints the values that were calcualted from the two pairs of coordinate points
     public String toString() {
         return "First pair: " + coord1 + "\nSecond pair: " + coord2 + "\nSlope of line: " + slope + "\nY-intercept: " + yInt() + "\nSlope intercept form: " + slopeIntForm() + "\nDistance between points: " + distanceCalc();
     }
